2ab4837a01b00cdd06e938af8fec8b4931d73497
Piotr Szczepaniak <pszczepaniak@uplandsoftware.com>
2020-08-04 15:28:43 +0200

    different solution

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -4,24 +4,19 @@ import ToDoItems from "../Components/ToDoItems";
 function Header() {
     return (
         <header>
-            {/*<nav className="navbar navbar-light bg-light">*/}
-            {/*<span className="navbar-brand">*/}
             Initial compiler redux
-            {/*</span>*/}
-            {/*    </nav>*/}
         </header>);
 }
 
 class ToDoList extends React.Component {
     constructor(props) {
         super(props);
-
+        this.inputRef = React.createRef(); //create reference for input box
         this.state = {
-            itemsArray: [] //array of items
+            itemsArray: []
         };
+        this.handleClick = this.handleClick.bind(this);         // This binding is necessary to make `this` work in the callback
 
-        // This binding is necessary to make `this` work in the callback
-        this.addTask = this.addTask.bind(this);
     }
 
     render() {
@@ -30,40 +25,35 @@ class ToDoList extends React.Component {
                 <div className="myHeader">
                     <Header/>
                 </div>
-                <div className="myInput">
-                    <form onSubmit={this.addTask}>
-                        <input ref={(a) => this.reffedInputElement = a}
-                               placeholder="Enter your task...">
-                        </input>
-                        <button type="submit">+</button>
-                    </form>
+                <div className="taskInput">
+                    <input
+                        id="taskInput"
+                        placeholder="Enter your task..."
+                        ref={this.inputRef}
+                    >
+                    </input>
+                    <button onClick={this.handleClick}>
+                        +
+                    </button>
                 </div>
-                <ToDoItems entries={this.state.itemsArray} />
+                <ToDoItems entries={this.state.itemsArray}/>
             </div>
         );
     }
 
-    addTask(e) {
-        e.preventDefault(); //overrides default behaviour of submit (page reload and clearing all)
+    handleClick() {
+        const input = this.inputRef;    //alternative: document.getElementById("taskInput");
 
-        if (this.reffedInputElement.value !== "") {
-            let newItem = {
-                text: this.reffedInputElement.value,
-                key: Date.now() //we use Date.now to give unique key for components
-            };
+        let newItem = {
+            text: input.current.value, //don't use "current" in alt version
+            key: Date.now() //we use Date.now to give unique key for components
+        };
 
-            // this.setState((prevState) => {
-            //     return {
-            //         itemsArray: prevState.itemsArray.concat(newItem)
-            //     };
-            // })
+        this.setState({
+            itemsArray: this.state.itemsArray.concat(newItem)
+        });
 
-            this.setState({
-                itemsArray: this.state.itemsArray.concat(newItem)
-            });
-
-            this.reffedInputElement.value = ""
-        }
+        input.current.value = "";
     }
 }
 
