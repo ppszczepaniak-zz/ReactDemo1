d5a220469d50ef871ac099331fadb7e71e24ad96
Piotr Szczepaniak <pszczepaniak@uplandsoftware.com>
2020-08-04 11:15:16 +0200

    add basic html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand">
+          Todo list, mohterfucker! xD
+
+        </span>
+            </nav>
+        </header>);
+}
+
+
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('root')
+);
